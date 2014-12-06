23a6dda8c65ff28418b619e27ff5cab362829070
Maxime Bittan <maxime.bittan@gmail.com>
2014-12-06 18:10:47 +0100

    Add some more file functions

--- include/gestion_fichier.h
+++ include/gestion_fichier.h
@@ -6,9 +6,14 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <dirent.h>
 
 #define SHAKESPEARE_DIR "./data/shakespeare/"
 #define DATA_DIR "./data/"
+
+#include "briandais.h"
+#include "trieHybride_primitives.h"
+#include "liste.h"
 
 /*
   Ouvre le fichier correspondant a nom. Retourne le descripteur de fichier
@@ -24,4 +29,20 @@ int ouvrir_fichier(char * nom);
 */
 int mot_suivant(int fd, char * mot);
 
+/*
+  Lit les mots du fichier passee en parametre et les ajoute dans le trie
+  hybride.
+*/
+TrieHybride * lire_fichier_th(char * fichier, TrieHybride * t);
+
+/*
+  Lit les mots du fichier et les ajoute dans l'arbre de la briandais passe en
+  parametre
+*/
+briandais_t * lire_fichier_briandais(char * fichier, briandais_t * t);
+
+/*
+  Rends la liste des fichiers reguliers dans le dossier passe en parametre
+*/
+Liste * fichiers_reguliers(char * dir);
 #endif
