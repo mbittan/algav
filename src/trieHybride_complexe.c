b67f0da19949967ee6618b4982f0bc4936aa60a8
Maxime Bittan <maxime.bittan@gmail.com>
2014-12-06 15:18:15 +0100

    Add convert function

--- src/trieHybride_complexe.c
+++ src/trieHybride_complexe.c
@@ -1,5 +1,29 @@
 #include "trieHybride_complexe.h"
 
 briandais_t * conversion(TrieHybride * t){
-  return NULL;
+  if(t==NULL){
+    return NULL;
+  }
+  briandais_t * b = conversion(t->inferieur);
+  briandais_t * b2, * aux;
+  if(t->fin){
+    b2 = new_briandais(t->c,new_empty_briandais(),NULL);
+    b2->son->brother=conversion(t->egal);
+  }else{
+    b2 = new_briandais(t->c,NULL,NULL);
+    b2->son=conversion(t->egal);
+  }
+  b2->son=conversion(t->egal);
+  b2->brother=conversion(t->superieur);
+
+  aux = b;
+  while(aux && aux->brother!=NULL){
+    aux=aux->brother;
+  }
+  if(aux==NULL){
+    return b2;
+  }else{
+    aux->brother=b2;
+  }
+  return b;
 }
